ad9ada6570012d5cdf50aa729f38ba8a38271a98
Vlad <VladT329@gmail.com>
2024-05-02 00:59:20 +0300

    3 done

--- src/Main.java
+++ src/Main.java
@@ -10,24 +10,32 @@ public class Main {
         System.out.println();
 
         System.out.println("Задание 2");
-        clientOS = 0;
-        int clientDeviceYear = 2015;
-        if (clientDeviceYear < 2015) {
-            if (clientOS == 0) {
-                System.out.println("Установите облегченную версию приложения для IOS по ссылке");
-            } else if (clientOS == 1) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            }
-        } else if (clientDeviceYear >= 2015) {
-            if (clientOS == 0) {
-                System.out.println("Установите версию приложения для IOS по ссылке");
-            } else if (clientOS == 1) {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            }
+        clientOS = 1;
+        int clientDeviceYear = 2014;
+        if (clientDeviceYear >= 2015 && clientOS == 0) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+        } else if (clientDeviceYear >= 2015 && clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        if (clientDeviceYear < 2015 && clientOS == 0) {
+            System.out.println("Установите облегчнную версию приложения для IOS по ссылке");
+        } else if (clientDeviceYear < 2015 && clientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
         System.out.println();
 
         System.out.println("Задание 3");
-
+        int year = 1584;
+        int userYear = 1984;
+        int difference = userYear - year;
+        if (difference % 4 == 0) {
+            if (difference % 100 == 0 && difference % 400 == 0) {
+                System.out.println("Високосный");
+            } else if (difference % 100 != 0) {
+                System.out.println("Високосный");
+            }
+        } else {
+            System.out.println("Не является високосным");
+        }
     }
 }
